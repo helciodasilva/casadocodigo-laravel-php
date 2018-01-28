61a467fe018a5cba60d533bbaeebe4e95c4a9f4c
Helcio da Silva <helciodasilva@gmail.com>
2018-01-28 10:59:01 -0200

    Capítulo 6

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+Route::get("/produtos/mostra/{id}", "ProdutoController@mostra")->where("id", "[0-9]+");
+
 Route::get("/produtos", "ProdutoController@lista");
 
 Route::get('/', function () {
